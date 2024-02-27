ff61640d2355c178d28a124de54efb74ea23e8c3
Johan Mari Ocampo <johanmari_ocampo@yahoo.com>
2024-02-28 07:58:50 +0800

    with displaying admin feedback

--- AdminFeedback.php
+++ AdminFeedback.php
@@ -1,23 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Got Funko Collections</title>
+    <title>Admin Feedback</title>
     <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-        
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Bowlby+One+SC&display=swap" rel="stylesheet">
-
     <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
     <div class="wrapper">
         <aside id="sidebar">
@@ -44,14 +40,58 @@
             <div class="sidebar-footer">
                 <a href="login.php" class="sidebar-link" title="Logout">
                     <i class="lni lni-exit"></i>
-                    
                 </a>
             </div>
         </aside>
 
         <div class="main p-3">
             <div class="text-center">
-            <h1 class="inventory-title">Admin Feedback</h1>
+                <h1 class="inventory-title">Admin Feedback</h1>
+            </div>
+
+            <div class="container">
+                <?php
+                // Connect to database
+                $conn = mysqli_connect('localhost', 'root', '', 'customer-feedback');
+
+                // Check connection
+                if (!$conn) {
+                    die("Connection failed: " . mysqli_connect_error());
+                }
+
+                // Retrieve feedbacks from the database
+                $sql = "SELECT * FROM customerfeedbacks";
+                $result = mysqli_query($conn, $sql);
+
+                if (mysqli_num_rows($result) > 0) {
+                    // Output data of each row
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        ?>
+                        <div class="card mb-3">
+                            <div class="card-header">
+                                Feedback from <?php echo $row['Email']; ?>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text"><?php echo $row['FeedbackContent']; ?></p>
+                                <!-- Add form for reply here -->
+                                <form method="post" action="reply_handler.php">
+                                    <input type="hidden" name="feedback_id" value="<?php echo $row['FeedbackID']; ?>">
+                                    <div class="form-group">
+                                        <label for="reply">Reply:</label>
+                                        <textarea class="form-control" id="reply" name="reply" rows="3"></textarea>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Reply</button>
+                                </form>
+                            </div>
+                        </div>
+                        <?php
+                    }
+                } else {
+                    echo "No feedbacks found.";
+                }
+
+                mysqli_close($conn);
+                ?>
             </div>
         </div>
     </div>
@@ -59,7 +99,5 @@
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
         crossorigin="anonymous"></script>
     <script src="AdminInventory.js"></script>
-    
 </body>
-
 </html>
